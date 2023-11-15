cc02275f2dc7ca32e64df7516e385c1f45211af7
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-11-16 01:32:31 +0900

    ✨ add IntentQQ

--- intent.go
+++ intent.go
@@ -9,22 +9,23 @@ const (
 	IntentDirectMessage            = 1 << 12
 	IntentOpenForumsEvent          = 1 << 18
 	IntentAudioOrLiveChannelMember = 1 << 19
+	IntentQQ                       = 1 << 25
 	IntentInteraction              = 1 << 26
 	IntentMessageAudit             = 1 << 27
 	IntentForumsEvent              = 1 << 28
 	IntentAudioAction              = 1 << 29
 	IntentPublicGuildMessages      = 1 << 30
 
-	// IntentAll 监听全部事件
-	IntentAll = IntentGuilds | IntentGuildMembers | IntentGuildMessages | IntentGuildMessageReactions |
+	// IntentAllGuild 监听全部频道事件
+	IntentAllGuild = IntentGuilds | IntentGuildMembers | IntentGuildMessages | IntentGuildMessageReactions |
 		IntentDirectMessage | IntentOpenForumsEvent | IntentAudioOrLiveChannelMember | IntentInteraction |
 		IntentMessageAudit | IntentForumsEvent | IntentAudioAction | IntentPublicGuildMessages
-	// IntentPublic 监听公域事件
-	IntentPublic = IntentGuilds | IntentGuildMembers | IntentGuildMessageReactions |
+	// IntentPublic 监听频道公域事件
+	IntentGuildPublic = IntentGuilds | IntentGuildMembers | IntentGuildMessageReactions |
 		IntentDirectMessage | IntentOpenForumsEvent | IntentAudioOrLiveChannelMember | IntentInteraction |
 		IntentMessageAudit | IntentAudioAction | IntentPublicGuildMessages
-	// IntentPrivate 监听私域事件
-	IntentPrivate = IntentGuilds | IntentGuildMembers | IntentGuildMessages | IntentGuildMessageReactions |
+	// IntentPrivate 监听频道私域事件
+	IntentGuildPrivate = IntentGuilds | IntentGuildMembers | IntentGuildMessages | IntentGuildMessageReactions |
 		IntentDirectMessage | IntentAudioOrLiveChannelMember | IntentInteraction |
 		IntentMessageAudit | IntentForumsEvent | IntentAudioAction
 )
